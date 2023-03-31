19ab6cf904ea92bbc7271b55ff71e873e44dace7
mrmurkur <mrmurkur@gmail.com>
2023-03-31 14:33:45 +1000

    added_ntp_1

--- opt_snmp_ubunt.py
+++ opt_snmp_ubunt.py
@@ -8,6 +8,7 @@ import os
 import datetime
 from tqdm import tqdm
 import re
+import csv
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
@@ -25,7 +26,8 @@ file_path_suc = "/home/milov/python/opt_snmp/log_snmp_cam_suc.txt"
 file_path_fail = "/home/milov/python/opt_snmp/log_snmp_cam_fail.txt"
 optimus_path_fail = "/home/milov/python/opt_snmp/log_optimus_snmp_cam_fail.txt"
 dahua_path_fail = "/home/milov/python/opt_snmp/log_dahua_snmp_cam_fail.txt"
-hikvision_file = "/home/milov/python/opt_snmp/hikvision.txt"
+hikvision_path_file = "/home/milov/python/opt_snmp/hikvision_cam_fail.txt"
+ntp_address = "10.78.0.89"
 
 with open('/home/milov/python/opt_snmp/cameras.txt', 'r') as f:
 # with open('/home/milov/python/opt_snmp/test.txt', 'r') as f:
@@ -42,6 +44,18 @@ def dahua_snmp(cam):
         el2.send_keys(Keys.RETURN)
         settings = WebDriverWait(driver, 25).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="main"]/ul/li[6]/span')))
         settings.click()
+        system_menu = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="set-menu"]/li[5]/a/span')))
+        system_menu.click()
+        system_basic = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="set-menu"]/li[5]/ul/li[1]/span')))
+        system_basic.click()
+        system_date = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, ' //*[@id="page_generalConfig"]/ul/li[2]')))
+        system_date.click()     
+        ntp = WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.XPATH, '//*[@id="gen_NTPServer"]')))
+        ntp.send_keys(Keys.CONTROL + "a")
+        ntp.send_keys(Keys.DELETE)        
+        ntp.send_keys(ntp_address)
+        accept_save = driver.find_element_by_xpath('//*[@id="page_generalConfig"]/div/div[2]/div[15]/a[3]')
+        accept_save.click()           
         net = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="set-menu"]/li[2]/a/span')))
         net.click()
         time.sleep(3)
@@ -95,8 +109,33 @@ def dahua_snmp(cam):
         with open(dahua_path_fail, 'a') as file:
             print(datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'), cam, "FAILED because", exc, file = file)
 
+def hikvision(link_to_cam):
+    try:
+        hik_login = WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.XPATH,'//*[@id="username"]')))
+        hik_login.send_keys('admin')
+        hik_passw = driver.find_element_by_xpath('//*[@id="password"]')
+        hik_passw.send_keys(pass_to_cam)
+        hik_passw.send_keys(Keys.RETURN)
+        hik_settings = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="nav"]/li[5]/a')))
+        hik_settings.click()
+        time.sleep(3)
+        hik_time_setup = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ui-id-2"]')))
+        driver.execute_script("arguments[0].click();", hik_time_setup)
+        hik_ntp_check = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="radioNTP"]')))
+        driver.execute_script("arguments[0].click();", hik_ntp_check)
+        hik_ntp = WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.XPATH, '//*[@id="settingTime"]/div/div[2]/div[3]/span[2]/input')))
+        hik_ntp.send_keys(Keys.CONTROL + "a")
+        hik_ntp.send_keys(Keys.DELETE)        
+        hik_ntp.send_keys(ntp_address)
+        hik_accept_save = driver.find_element_by_xpath('//*[@id="settingTime"]/button')
+        hik_accept_save.click()
+        with open(file_path_suc, 'a') as file:
+            print(datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'), link_to_cam, "hikvision ntp enabled", file = file)
+    except Exception as exc:
+        with open(hikvision_path_file, 'a') as file:
+            print(datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'), link_to_cam, "FAILED because", exc, file = file)
 
-def optimus_snmp(cam):
+def optimus_snmp_ntp(cam):
     try:
         driver.get("http://" + cam)
         opt_login = WebDriverWait(driver, 25).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[1]/div/div/input')))
@@ -106,8 +145,17 @@ def optimus_snmp(cam):
         opt_passw.send_keys(pass_to_cam)
         time.sleep(1)
         opt_passw.send_keys(Keys.RETURN) 
-        opt_settings = WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="mainContainer"]/section/header/div[2]/div/div[1]/div/ul/li[1]/div')))
-        opt_settings.click()
+        opt_settings = WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.XPATH, '//*[@id="mainContainer"]/section/header/div[2]/div/div[1]/div/ul/li[1]/div')))
+        driver.execute_script("arguments[0].click();", opt_settings)
+        opt_basic = WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.XPATH, '//*[@id="remoteSetting"]/section/aside/div/div[6]/div[2]/div/ul/li[1]')))
+        driver.execute_script("arguments[0].click();", opt_basic)
+        time.sleep(1)
+        opt_ntp = WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.XPATH, '//*[@id="subPage"]/div[1]/div/div/div[6]/div/div[2]/input')))
+        opt_ntp.send_keys(Keys.CONTROL + "a")
+        opt_ntp.send_keys(Keys.DELETE)        
+        opt_ntp.send_keys(ntp_address)
+        opt_accept_save = driver.find_element_by_xpath('//*[@id="subPage"]/div[2]/div/button[1]')
+        opt_accept_save.click()        
         try:
             opt_net_basic = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="remoteSetting"]/section/aside/div/div[4]/div[2]/div/ul/li[1]')))
             opt_net_basic.click()
@@ -148,14 +196,13 @@ def first_look(link_to_cam):
             html = page.read().decode("utf-8")
             soup = BeautifulSoup(html, "html.parser")
             if soup.title.string == "webpackSPA":
-                optimus_snmp(link_to_cam)
+                optimus_snmp_ntp(link_to_cam)
             else:
                 driver.get("http://" + link_to_cam)
                 try:
                     naming = driver.find_element(By.CLASS_NAME, "footer")
                     if re.findall(r'Hikvision', naming.text):
-                        with open(hikvision_file, 'a') as file:
-                            print(link_to_cam, file = file)
+                        hikvision(link_to_cam)
                 except:
                     dahua_snmp(link_to_cam)
         except Exception as exc:
